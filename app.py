454a55e362f386fa6d2fd8528e566aa57afbfaca
NetKar <133479529+NetKar@users.noreply.github.com>
2025-10-29 22:06:28 +0530

    Update app.py 7

--- app.py
+++ app.py
@@ -165,6 +165,7 @@ def recognize_gesture():
         "meaning": mudra_details['meaning']
     })
 
+initialize()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(debug=False, host='0.0.0.0', port=port)
